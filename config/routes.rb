76d188c29005dbddd8c107d118d244d20dfc0d20
rata854 <nitiham2006@gmail.com>
2021-06-08 01:40:44 +0000

    [Finish]bookers7c

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  resources :groups, only: [:new, :index, :create, :edit, :update]
+  resources :groups
 end
